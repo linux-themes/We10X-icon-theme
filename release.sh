1796d9a5ac18cbbf793ff6d16757b3dcac272ca5
jordan <sudo.sendmail.jordan@gmail.com>
2025-02-16 19:00:32 -0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,9 @@
 mkdir -p $PWD/release
 $PWD/install.sh -a -d $PWD/release
-for i in $PWD/release/*; do tar -czvf "${i%/}.tar.gz" "$i"; done
+tar -czvf $PWD/release/We10X.tar.xz $PWD/release
+for directory in $PWD/release/*; do
+  if [[ "$directory" == *"We10X.tar.xz"* ]]; then
+    continue
+  fi
+  tar -czvf "${directory%/}.tar.xz" "$directory"
+done
